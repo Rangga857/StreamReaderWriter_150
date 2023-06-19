ca558057f77995641c5a249d12e73f0fb91bf7cc
Rangga857 <azharrangga28@gmail.com>
2023-06-19 10:59:50 +0700

    membuka file dalam mode menulis.

--- Iofilewithpatch/Iofilewithpatch.cpp
+++ Iofilewithpatch/Iofilewithpatch.cpp
@@ -6,4 +6,10 @@ using namespace std;
 int main() {
 	string baris;
 	string NamaFile;
+
+	cout << "Masukkan Nama file : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
 }
