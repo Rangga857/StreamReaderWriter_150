3348167e1ef9c2abfe2220faef8c3749bb0cbc42
Rangga857 <azharrangga28@gmail.com>
2023-06-19 11:03:19 +0700

    loop akan berhenti jika memasukan karakter q

--- Iofilewithpatch/Iofilewithpatch.cpp
+++ Iofilewithpatch/Iofilewithpatch.cpp
@@ -22,6 +22,8 @@ int main() {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika memasukan karakter q
+		if (baris == "q") break;
 
 	}
 }
