31bdd8ec98fda96593e7163da9b1d5bd8b646d58
Rangga857 <azharrangga28@gmail.com>
2023-06-19 11:25:01 +0700

    akan dieksekusi karena array datahanya memiliki 3 elemen

---
+++ Exceptionstandar/Exceptionstandar.cpp
@@ -0,0 +1,20 @@
+#include<exception>
+#include<array>
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Awal Program" << endl;
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan arrar integer 3 elemen
+		//cout << data.at(5)<< endl
+		//memanggil array elemen ke 5
+	}
+	catch(exception &e){
+		//penangkap menggunakan  obyek exceptiom
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array datahanya memiliki 3 elemen*/
+	}
+}
