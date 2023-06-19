7749229f627fadd541b53cecf84907ccba12ab28
Rangga857 <azharrangga28@gmail.com>
2023-06-19 11:08:00 +0700

    melakukan  perulangan setiap baris

--- Iofilewithpatch/Iofilewithpatch.cpp
+++ Iofilewithpatch/Iofilewithpatch.cpp
@@ -33,4 +33,16 @@ int main() {
 
 	//membuka file dalam mode membaca
 	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile, ios::out);
+
+	cout << endl << "=> Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open()) {
+		//melakukan  perulangan setiap baris
+		while (getline(infile, baris)) {
+
+		}
+
+	}
 }
