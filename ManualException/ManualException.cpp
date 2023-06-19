09831f121071c30a7577d5a25f9e40d677e95e84
Rangga857 <azharrangga28@gmail.com>
2023-06-19 11:15:56 +0700

    jika pada throw dinonaktifkan maka akan tampil "Pernyataan tidak akan berhasil"

---
+++ ManualException/ManualException.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	try{
+		cout << "Selamat belajar di Prodi TI UMY" << endl;
+		//throw 0,5; //melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan berhasil";
+	}
+	catch (int a) {
+		//blok ini 
+	}
+}
