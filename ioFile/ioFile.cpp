7b2751c941d9fbfae8efaac2bd5b8f0d3ecc6764
Rangga857 <azharrangga28@gmail.com>
2023-06-19 10:23:48 +0700

    loop untuk menulis

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -10,5 +10,11 @@ int main() {
 	//menunjuk ke sebuah nama file.
 	outfile.open("contohfile.txt");
 
+	cout << "=>Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+
+	}
 }
 	
