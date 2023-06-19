fbcf430242264503a8c8af69ef57406f1f5fcb0b
Rangga857 <azharrangga28@gmail.com>
2023-06-19 10:37:47 +0700

    melakukan perulangan setiap baris

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -30,6 +30,15 @@ int main() {
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
 
+	cout << endl << "=> Membuka dan membaca file " << endl;
+	//jike file ada maka
+	if (infile.is_open()) {
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris)) {
+
+		}
+	}
+
 
 
 }
